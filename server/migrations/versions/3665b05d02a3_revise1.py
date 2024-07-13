5eca41fb2ffbc25c9b1765a276ff56bfd505a6ef
Debbie30 <cherotichdebby11@gmail.com>
2024-07-14 02:20:53 +0300

    done

---
+++ server/migrations/versions/3665b05d02a3_revise1.py
@@ -0,0 +1,24 @@
+"""revise1
+
+Revision ID: 3665b05d02a3
+Revises: ba76cbc2339a
+Create Date: 2024-07-09 03:04:34.646693
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3665b05d02a3'
+down_revision = 'ba76cbc2339a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
